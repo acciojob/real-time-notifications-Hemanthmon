ec080aeca55f5af9880eb34bc68cd37f52aba1a3
Hemanth V <153289923+Hemanthmon@users.noreply.github.com>
2025-04-04 13:19:28 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-//your JS code here. If required.
-function ascendingOrder(employ){
-    employ.sort((a,b) => a.age - b.age)
-	 return employ.map(obj => `${obj.id},${obj.name}, ${obj.age}, ${obj.contact} `)
-}
-console.log(ascendingOrder(employ))
